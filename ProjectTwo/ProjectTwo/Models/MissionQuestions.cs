ec5300db72f0738783ce1935838a3e2625f52ccc
twandry <tdwandry@gmail.com>
2018-12-12 17:16:18 -0700

    added missionQuestions model

---
+++ ProjectTwo/ProjectTwo/Models/MissionQuestions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTwo.Models
+{
+   public class MissionQuestions
+   {
+      Table["MissionQuestions"]
+      public class MissionQuestions
+      {
+         [Key]
+         public int missionQuestionID { get; set; }
+
+         public string question { get; set; }
+         public string answer { get; set; }
+
+         public virtual int? missionID {get;set;}
+         public virtual int? userID { get; set; }
+      }
+   }
+}
