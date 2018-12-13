6fa3b603b466e39d60f5784bb76633c11daf97ab
curnowbr <curnowbr@gmail.com>
2018-12-12 18:45:22 -0700

    images

---
+++ ProjectTwo/ProjectTwo/Controllers/MissionsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectTwo.DAL;
+using ProjectTwo.Models;
+
+namespace ProjectTwo.Controllers
+{
+    public class MissionsController : Controller
+    {
+        private ProjectTwoContext db = new ProjectTwoContext();
+
+        // GET: Missions
+        public ActionResult Index()
+        {
+            return View(db.Missions.ToList());
+        }
+
+        // GET: Missions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Missions missions = db.Missions.Find(id);
+            if (missions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(missions);
+        }
+
+        // GET: Missions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Missions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "missionID,missionName,missionPresName,missionAddress,language,climate,domReligion")] Missions missions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Missions.Add(missions);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(missions);
+        }
+
+        // GET: Missions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Missions missions = db.Missions.Find(id);
+            if (missions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(missions);
+        }
+
+        // POST: Missions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "missionID,missionName,missionPresName,missionAddress,language,climate,domReligion")] Missions missions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(missions).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(missions);
+        }
+
+        // GET: Missions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Missions missions = db.Missions.Find(id);
+            if (missions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(missions);
+        }
+
+        // POST: Missions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Missions missions = db.Missions.Find(id);
+            db.Missions.Remove(missions);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ProjectTwo/ProjectTwo/Models/Missions.cs
+++ ProjectTwo/ProjectTwo/Models/Missions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -13,12 +14,18 @@ namespace ProjectTwo.Models
         [Key]  
          public int missionID { get; set; }
 
+        [DisplayName("Mission Name")]
         public string missionName { get; set; }
+        [DisplayName("Mission President Name")]
         public string missionPresName { get; set; }
+        [DisplayName("Mission Address")]
         public string missionAddress { get; set; }
+        [DisplayName("Mission Language")]
         public string language { get; set; }
+        [DisplayName("Mission Climate")]
         public string climate { get; set; }
-        public string domReligon { get; set; }
+        [DisplayName("Dominant Religion")]
+        public string domReligion { get; set; }
 
     
     }
