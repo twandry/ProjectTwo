dc8cb3b2258a7cbc35aeffb2a3b815b693eb600d
curnowbr <curnowbr@gmail.com>
2018-12-12 19:09:43 -0700

    F.A.Qs

--- ProjectTwo/ProjectTwo/Models/MissionQuestions.cs
+++ ProjectTwo/ProjectTwo/Models/MissionQuestions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,7 +16,9 @@ namespace ProjectTwo.Models
          [Key]
          public int missionQuestionID { get; set; }
 
+        [DisplayName("Question")]
          public string question { get; set; }
+        [DisplayName("Answer")]
          public string answer { get; set; }
 
          public virtual int? missionID {get;set;}
