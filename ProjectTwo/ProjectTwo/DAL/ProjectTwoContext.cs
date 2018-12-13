64e19ce848adecdeb4e9dd39f574b046dfdd3c21
curnowbr <curnowbr@gmail.com>
2018-12-12 17:14:10 -0700

    newmodels

---
+++ ProjectTwo/ProjectTwo/DAL/ProjectTwoContext.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTwo.DAL
+{
+    public class ProjectTwoContext : DbContext
+    {
+        public ProjectTwoContext() : base("ProjectTwoContext")
+        {
+
+        }
+        public DbSet<Users>Users { get; set; }
+    }
+}
---
+++ ProjectTwo/ProjectTwo/Models/Missions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTwo.Models
+{
+    [Table("Missions")]
+    public class Missions
+    {
+        [Key]  
+         public int missionID { get; set; }
+
+        public string missionName { get; set; }
+        public string missionPresName { get; set; }
+        public string missionAddress { get; set; }
+        public string language { get; set; }
+        public string climate { get; set; }
+        public string domReligon { get; set; }
+
+    
+    }
+}
